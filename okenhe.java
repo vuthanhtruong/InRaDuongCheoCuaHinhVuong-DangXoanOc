d95981315f2d1cf3d9424b82e4a6e3cac8f18b5a
Vũ Thanh Trường <133890957+vuthanhtruong@users.noreply.github.com>
2024-05-21 00:22:41 +0700

    Update okenhe.java

--- okenhe.java
+++ okenhe.java
@@ -26,5 +26,61 @@ public class truongday {
       c += 2;
     }
     System.out.println("Gia tri cua sum la: " + sum);
+    System.out.println("______________________________________________________________________");
+
+    System.out.println("Moi ban nhap canh hinh vuong");
+		int n=new Scanner(System.in).nextInt();
+		int M[][]=new int[n][n];
+		int h1=0;
+		int h2=n-1;
+		int c1=0;
+		int c2=n-1;
+		int count=1;
+		while(h1<=h2 && c1<=c2) {
+			for(int i=c1;i<=c2;i++) {
+				M[h1][i]=count;
+				count+=2;
+			}
+			h1++;
+			for(int j=h1;j<=h2;j++) {
+				M[j][c2]=count;
+				count+=2;
+			}
+			c2--;
+			for(int m=c2;m>=c1;m--) {
+				M[h2][m]=count;
+				count+=2;
+			}
+			h2--;
+			for(int k=h2;k>=h1;k--) {
+				M[k][c1]=count;
+				count+=2;
+			}
+			c1++;
+		
+		}
+		for(int i = 0; i < n; i++){
+	        for(int j = 0; j < n; j++){
+	            System.out.print(M[i][j]+"   ");
+	        }
+	        System.out.println();
+	    }
+		System.out.println("____________");
+	    for(int i = 0; i < n; i++){
+	        for(int j = 0; j < n; j++){
+	        	if(i==j) {
+	            System.out.print(M[i][j]+"   ");
+	        	}
+	        }
+	    }
+	    System.out.println();
+	    System.out.println("____________");
+	    for(int i = 0; i < n; i++){
+	        for(int j = 0; j < n; j++){
+	        	if(n-j==i+1) {
+	            System.out.print(M[i][j]+"   ");
+	        	}
+	        }
+	    }
   }
 }
